53df53ba78079962a5c3b8f9e5655423280dd423
SHY-KG <tjdghks3078etc@gmail.com>
2019-11-19 22:55:37 +0900

    Updates

---
+++ precache-manifest.97a296244c6b5e0ff7b825f77891d47e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d83368b20c671401318b34aa7d2908e",
+    "url": "/react_top20movies/index.html"
+  },
+  {
+    "revision": "6688cf2d09768075337b",
+    "url": "/react_top20movies/static/js/2.830d2a7b.chunk.js"
+  },
+  {
+    "revision": "21781e4c6b9e91f52baa",
+    "url": "/react_top20movies/static/js/main.32a80139.chunk.js"
+  },
+  {
+    "revision": "cddb7d6acf4967c114e2",
+    "url": "/react_top20movies/static/js/runtime-main.f07a4606.js"
+  }
+]);
